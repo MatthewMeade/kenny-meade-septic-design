11ebacfc56e5ae73499f66baabc9a6472bbc87a8
Matthew Meade <email@matthewmeade.ca>
2025-06-10 21:54:15 -0230

    Remove tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from "./pages/Index";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
     <Index />
-  </QueryClientProvider>
 );
 
 export default App;
